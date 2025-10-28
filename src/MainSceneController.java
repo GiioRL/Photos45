008f2cca54bb539aa16dbd36d5d9e832648b63c7
GiioRL <meoweycupentcmc@gmail.com>
2025-10-28 18:01:53 -0400

    made a sample program

---
+++ src/MainSceneController.java
@@ -0,0 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class MainSceneController {
+
+    @FXML
+    private TextField titleTF;
+
+    @FXML
+    void butonClick(ActionEvent event) {
+        System.out.println("CHAT HE CLICK BUTON!!!");
+        Stage mainWindow = (Stage) titleTF.getScene().getWindow();
+        String title = titleTF.getText();
+        mainWindow.setTitle(title);
+    }
+
+}
